ec1abc0701a82bf8b8f9833cfa6c3ca01ec198af
Jesus-58 <140073012+Jesus-58@users.noreply.github.com>
2023-12-15 20:41:17 +0100

    Create ft_strnstr.c

---
+++ ft_strnstr.c
@@ -0,0 +1,34 @@
+char	*ft_strnstr(char *s1, char *s2, size_t n)
+{
+	if (s2 == '\0')
+		return (s1);
+	while (n-- && strlen(s2) >= n)
+	{
+		if (*s2 == *s1)
+			return (s2);
+		s1++;
+		s2++;
+	}
+	return (NULL);
+}
+
+/*
+#include <stdio.h>
+#include "libft.h"
+
+int main() {
+    char haystack[] = "Hello, world!";
+    char needle1[] = "world";
+    char needle2[] = "planet";
+    char needle3[] = "";
+
+    char *result1 = ft_strnstr(haystack, needle1, sizeof(haystack));
+    char *result2 = ft_strnstr(haystack, needle2, sizeof(haystack));
+    char *result3 = ft_strnstr(haystack, needle3, sizeof(haystack));
+
+    printf("Resultado 1: %s\n", result1 ? result1 : "NULL");
+    printf("Resultado 2: %s\n", result2 ? result2 : "NULL");
+    printf("Resultado 3: %s\n", result3 ? result3 : "NULL");
+
+    return 0;
+}*/
