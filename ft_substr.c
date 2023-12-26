4ed0ed2d044fbf239d393e1d1445b13792f8a016
Jesus-58 <140073012+Jesus-58@users.noreply.github.com>
2023-12-26 18:42:01 +0100

    Add files via upload

---
+++ ft_substr.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_substr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jesumore <jesumore@student.42malaga.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/26 18:31:15 by jesumore          #+#    #+#             */
+/*   Updated: 2023/12/26 18:41:29 by jesumore         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_substr(char const *s, unsigned int star, size_t len)
+{
+	char *result;
+
+	result = (char *)malloc(sizeof(char) * len);
+	if (result = NULL)
+		return (NULL);
+	while (len > 0)
+	{
+		
+		len--;
+	}
+}
