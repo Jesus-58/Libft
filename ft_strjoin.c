d7221e4afa597c69c9e6b3941e4914b6d36674cf
Jesus-58 <jesusmorencanonne@gmail.com>
2024-01-09 18:09:51 +0100

    ft_strjoin

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: jesumore <jesumore@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/03 20:04:06 by jesumore          #+#    #+#             */
-/*   Updated: 2024/01/06 21:34:23 by jesumore         ###   ########.fr       */
+/*   Updated: 2024/01/09 13:31:21 by jesumore         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,14 +16,42 @@ char	*ft_strjoin(char const *s1, char const *s2)
 {
 	char	*result;
 	int	len;
+	char	*copy_s1;
+	char	*copy_s2;
 
-	len = ft_strlen(s1) + ft_strlen(s2);
 	if (s1 && s2)
 	{
+		len = strlen(s1) + strlen(s2);
+		
 		if (!(result = (char *)malloc(sizeof(char) * (len + 1))))
 			return(NULL);
-		result = strlcat(s1, s2, ft_strlen(s2));
+		copy_s1 = strdup(s1);
+		copy_s2 = strdup(s2);
+		strlcpy(result, copy_s1, len + 1);
+		strlcat(result, copy_s2, len + 1);
+		free(copy_s1);
+		free(copy_s2);
 		return (result);
 	}
 	return (NULL);
+}
+#include <stdio.h>
+
+int main()
+{
+	char *s1 = "Hello, ";
+	char *s2 = "world!";
+	char *result = ft_strjoin(s1, s2);
+	
+	if (result != NULL)
+	{
+		printf("Result: %s\n", result);
+		free(result);
+	}
+	else
+	{
+		printf("Error: Failed to allocate memory.\n");
+	}
+	
+	return 0;
 }
