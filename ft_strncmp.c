f5dc485730446992b7c02b9563b3d69783cf1bb0
Jesus-58 <140073012+Jesus-58@users.noreply.github.com>
2024-01-31 11:19:49 +0100

    Update ft_strncmp.c

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -14,11 +14,9 @@
 
 int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
-	size_t	value;
 	size_t	i;
 
 	i = 0;
-	value = 0;
 	while ((s1[i] != '\0' || s2[i] != '\0') && i < n)
 	{
 		if (s1[i] != s2[i])
